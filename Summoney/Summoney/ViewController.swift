cd95ab9943c59d11aaf27b13481bb3bd93921025
ThienPham <thienphamios@gmail.com>
2018-04-07 23:52:53 +0700

    add project

---
+++ Summoney/Summoney/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Summoney
+//
+//  Created by Thien Pham on 4/7/18.
+//  Copyright © 2018 Thien Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
